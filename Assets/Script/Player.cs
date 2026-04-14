9a71178b384cefba14c2c224b8fbfc4785f4f180
gk <pengpham@gmail.com>
2026-04-14 00:42:01 -0400

    basic player movement

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float PlayerSpeed;
+    private Rigidbody2D rb;
+
+    private Vector2 movement;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        if (rb == null)
+        {
+            Debug.LogError("No Rigidbody2D found on this GameObject!");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        movement = movement.normalized;
+    }
+
+    void FixedUpdate()
+    {
+        rb.linearVelocity = movement * PlayerSpeed;
+    }
+}
